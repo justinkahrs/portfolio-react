91ece3f68cf4cda2f673f0ec37873f84e16a723a
avanderj <avanderj@indiana.edu>
2017-10-30 20:53:56 -0400

    split pages

---
+++ src/PortfolioPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class PortfolioPage extends Component {
+  render() {
+    return (
+      <div class="pageThree" id="p3">
+        <div class="content text-center">
+          <h1>Portfolio</h1>
+          <h2>Check out some of my recent projects, ideas, and designs</h2>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6">
+                <h2>My Portfolio</h2>
+              </div>
+
+              <div class="col-md-6">
+                <h2>My Portfolio</h2>
+              </div>
+
+              <div class="col-md-6">
+                <h2>My Portfolio</h2>
+              </div>
+
+              <div class="col-md-6">
+                <h2>My Portfolio</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PortfolioPage;
---
+++ src/ResumePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class ResumePage extends Component {
+  render() {
+    return (
+      <div class="pageFour" id="p4">
+        <div class="content text-center">
+          <h1>Resume</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ResumePage;
