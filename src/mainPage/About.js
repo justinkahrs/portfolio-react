e469ad10c4ca0230255dc06a7d07db6c8f1cdceb
Justin Kahrs <justin@justinkahrs.com>
2018-07-08 12:02:59 -0700

    about page

--- src/mainPage/About.js
+++ src/mainPage/About.js
@@ -11,35 +11,48 @@ class About extends Component {
       minHeight: '60vh',
     },
     bq: {
+      fontFamily: 'Libre Baskerville',
       fontWeight: 'bold',
+      fontSize: '24px',
+      marginLeft: 0,
     },
     header: {
+      fontSize: '18px',
       textTransform: 'uppercase',
-      textAlign: 'center',
-      padding: this.props.big ? '' : '3rem',
     },
     text: {
       color: '#212C21',
-      padding: this.props.big ? '1rem 45rem' : '0 3rem',
+      padding: this.props.big ? '1rem 25%' : '0 3rem',
+    },
+    hr: {
+      background: 'black',
+      border: 0,
+      height: '2px',
+      maxWidth: '100px',
     },
   });
   render() {
     return (
       <div id="about" style={this.styles().aboutPage}>
-        <h1 style={this.styles().header}>A little bit about myself</h1>
+        <h1 style={this.styles().header}>
+          A little bit about myself
+          <hr style={this.styles().hr} />
+        </h1>
         <div style={this.styles().text}>
           <p>
             I have a passion for creating engaging digital experiences through
-            user research and information architecture. With degrees in both art
-            and information science, I am obsessed with visual goodness and the
-            details.
+            user-centered design and information architecture. With degrees in
+            both art and information science, I am obsessed with visual goodness
+            and systemic details.
           </p>
           <blockquote style={this.styles().bq}>
             I enjoy finding that sweet spot where form meets function.
           </blockquote>
           <p>
-            Recently, I've taken a deep dive into creative coding with JS and D3
-            to broaden my prior knowledge of web technologies like HTML and CSS.
+            Besides UX, I've taken a deep dive into creative coding with JS and
+            D3 to broaden my prior knowledge of web technologies like HTML and
+            CSS. When I'm not staring at a screen, I enjoy pinball and my cat.
+            Just look at her.
           </p>
         </div>
       </div>
