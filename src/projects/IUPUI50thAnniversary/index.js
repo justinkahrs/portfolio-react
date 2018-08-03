ebf27052b915538c4d49e83c24afbca34fff7186
Justin Kahrs <justin@justinkahrs.com>
2018-08-02 21:50:52 -0700

    use optimize images for 50th, update carousel

--- src/projects/IUPUI50thAnniversary/index.js
+++ src/projects/IUPUI50thAnniversary/index.js
@@ -4,13 +4,13 @@ import postit1 from './images/50anniversary-postit1.png'
 import postit2 from './images/50anniversary-postit2.png'
 import postit3 from './images/50anniversary-postit3.png'
 import siteMap from './images/50anniversary-sitemap.png'
-import landingPage from './images/landingPage.png'
-import events from './images/events.png'
-import ourHistory from './images/ourHistory.png'
-import stories from './images/stories.png'
-import fiftyFaces from './images/fiftyFaces.png'
-import getInvolved from './images/getInvolved.png'
-import contact from './images/contact.png'
+import slide1 from './images/slide1-landingPage-optimized.png'
+import slide2 from './images/slide2-events-optimized.png'
+import slide3 from './images/slide3-ourHistory-optimized.png'
+import slide4 from './images/slide4-stories-optimized.png'
+import slide5 from './images/slide5-fifty-faces-optimized.png'
+import slide6 from './images/slide6-getInvolved-optimized.png'
+
 import ixd1 from './images/50anniversary-ixd1.png'
 import ixd2 from './images/50anniversary-ixd2.png'
 
@@ -25,13 +25,12 @@ export default {
   processPics: [postit1, postit2, postit3],
   siteMap,
   slideshowPics: [
-    landingPage,
-    events,
-    ourHistory,
-    stories,
-    fiftyFaces,
-    getInvolved,
-    contact,
+    slide1,
+    slide2,
+    slide3,
+    slide4,
+    slide5,
+    slide6    
   ],
   whiteBoardPics: [ixd1, ixd2],
 }
