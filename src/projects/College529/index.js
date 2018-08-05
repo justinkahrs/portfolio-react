06f3ea598c63d6a93bfe42ee74097035a15f2c0b
Justin Kahrs <justin@justinkahrs.com>
2018-08-05 12:43:34 -0700

    529 optimized

--- src/projects/College529/index.js
+++ src/projects/College529/index.js
@@ -1,8 +1,8 @@
-import thumbnail from './images/529-thumb.png'
-import hero from './images/529-hero.png'
-import taskPic from './images/529-formOutput.png'
-import taskPic2 from './images/529-reportFindings.png'
-import sitemap from './images/529-sitemap.png'
+import thumbnail from './images/529-thumb-optimized.png'
+import hero from './images/529-hero-optimized.png'
+import taskPic from './images/529-formOutput-optimized.jpg'
+import taskPic2 from './images/529-reportFindings-optimized.jpg'
+import sitemap from './images/529-sitemap-optimized.jpg'
 export default {
   id: 3,
   date: '2017-2018',
