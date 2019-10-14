d2df968116abf43c46c9a067b52aa9c31fd43de0
Anna VanderJagt <21692318+avanderj@users.noreply.github.com>
2019-10-14 13:52:33 -0700

    Update Page.js

--- src/projects/HelpCenter/Page.js
+++ src/projects/HelpCenter/Page.js
@@ -66,7 +66,7 @@ class Page extends Component {
         <Header title={'Project context'} />
         <TextContainer>
           <React.Fragment>
-          Libraries are complex institutions filled with information and one of the ongoing challenges is connecting users to the people and resources they need. <strong>The reality is that many users want to find information on their own.</strong> Zendesk Guide provides a great way to organize and share resources and empowers users to find what they need on their own. 
+          Libraries are complex institutions filled with information. One of the ongoing challenges is connecting users to the people and resources they need. <strong>The reality is that many users want to find information on their own.</strong> Zendesk Guide provides a great way to organize and share resources and empowers users to find what they need on their own. 
 How a knowledge base is designed affects customers' experience with your brand. A knowledge base is only as helpful as its content and when users find that content is outdated or incorrect, that’s a reflection of the brand and its credibility to provide good service. 
 At UCSF, the library’s knowledge base was largely outdated, did not have an assigned owner, and was very underutilized.
 
@@ -75,28 +75,28 @@ At UCSF, the library’s knowledge base was largely outdated, did not have an as
         <Header title={'Goals and outcomes'} />
         <TextContainer>
           <React.Fragment>
-          <strong>Identify knowledge gaps.</strong> Audited existing content, reached out to stakeholders for fresh content, and research support ticket history to identify information gaps within the existing knowledge base.
+          <strong>Identify knowledge gaps</strong> – Audited existing content, reached out to stakeholders for fresh content, and research support ticket history to identify information gaps within the existing knowledge base.
 
           </React.Fragment>
           <React.Fragment>
-          <strong>Information architecture. </strong>Mapped out the existing architecture and benchmarked library knowledge base architecture.
+          <strong>Information architecture</strong> – Mapped out the existing architecture and benchmarked library knowledge base architecture.
           </React.Fragment>
         <ImageContainer big={big} pics={architectureHC} />
         </TextContainer>
         <TextContainer>
           <React.Fragment>
-          <strong>Reduce cognitive load on the user.</strong>  Incorporated a clear architecture that’s jargon-free with a narrow, deep architecture approach.
+          <strong>Reduce cognitive load on the user</strong> – Incorporated a clear architecture that’s jargon-free with a narrow, deep architecture approach.
 
           </React.Fragment>
           <React.Fragment>
-          <strong>Consistent look and feel. </strong>Selected new Zendesk theme and created style guide for writing for the web and formatting content.
+          <strong>Consistent look and feel</strong> – Selected new Zendesk theme and created style guide for writing for the web and formatting content.
           </React.Fragment>
           <ImageContainer big={big} pics={titlesHC} />
           <React.Fragment><div style={this.styles().caption}>Consistent article titles. Question? Answer.</div></React.Fragment>
           <ImageContainer big={big} pics={styleHC} />
           <React.Fragment><div style={this.styles().caption}>Style guide incorporated in the Help Center for easy reference.</div></React.Fragment>
         <React.Fragment>
-          <strong>Integrate with existing tools/platforms </strong>Provided multiple portals to the Help Center throughout main website and incorporated existing support tool so stakeholders answering support tickets can easily access documentation.
+          <strong>Integrate with existing tools/platforms</strong> – Provided multiple portals to the Help Center throughout main website and incorporated existing support tool so stakeholders answering support tickets can easily access documentation.
           </React.Fragment>
           <ImageContainer big={big} pics={gif} />
           <React.Fragment><div style={this.styles().caption}>Knowledge Capture tool allows Zendesk agents to easily see if the answer to a patron’s inquiry  is already in the Help Center. When replying to tickets, this tool will search and link to relevant documentation directly within the text editor instead of a separate browser window.</div></React.Fragment>
